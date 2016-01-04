308df1ea48bfbce325bf1f0c620672b3d067b77c
João Batista Silva <joao@faustino.eti.br>
2016-01-04 00:13:23 -0400

    Some cleaning at player.rb

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,2 @@
-class Player < ActiveRecord::Base
-  
-  def new_turn
-    self.turn = self.turn + 1
-    self.turn_score = 0
-    self.roll_score = 0
-    self.dice = 5
-  end
-    
+class Player < ActiveRecord::Base    
 end
